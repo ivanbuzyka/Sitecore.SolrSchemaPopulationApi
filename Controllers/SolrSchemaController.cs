f632c4385b2e488a1b846d2501decf245a66dbc1
ivanbuzyka <ivanbuzyka@gmail.com>
2022-09-09 16:21:44 +0200

    initial commit

---
+++ Controllers/SolrSchemaController.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Maintenance;
+using Sitecore.Services.Infrastructure.Security;
+using System.Web.Http;
+
+namespace SolrSchemaPopulationApi.Controllers
+{
+  [RoutePrefix("api/solrschemamaintenance")]
+  [RequiredApiKey]
+  public class SolrSchemaController : ApiController
+  {
+    [HttpGet]
+    [Route("populate")]
+    public IHttpActionResult Populate()
+    {
+      foreach (ISearchIndex index in ContentSearchManager.Indexes)
+      {
+        SchemaCustodian.PopulateManagedSchema(index);
+      }
+
+      return Ok("Done");
+    }
+  }
+}
